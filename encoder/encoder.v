1ecf17386d938b9bbeb52150a49a0f5c69716489
Owen Smith <owen.smith2@duke.edu>
2019-02-26 17:52:39 -0500

    Made some updates to verilog, but getting a register compilation error that I don't understand

--- encoder/encoder.v
+++ encoder/encoder.v
@@ -1,15 +1,38 @@
 `timescale 1 ps / 1 ps
-module encoder (clr, enable, u, clk, Xk, Zk);
+module encoder (clr, enable, u, tOut, tIn, clk, Xk, Zk);
 
-input clr, enable, u;
-output [15:0] Xk, Zk, Zprimek;
+input clr, enable, u, tOut, clk;
+output Xk, Zk, tIn;
+
+reg switchWire;
 
 wire Q2, Q1, Q0;
-wire zkPrior;
+wire xor3, xor2, xor1, xor0; 
 
-dffe_ref D2(.q(Q2), .d(u), .clk(clk), .en(enable), .clr(clr));
+always @(posedge clk)
+
+//assuming the enable signal is the switch? 
+
+if(~enable) begin
+	switchWire <= tOut;
+end else begin
+	switchWire <= u;
+end 
+
+assign Xk = switchWire;
+
+xor xorGate2(xor2,xor3,switchwire);
+
+dffe_ref D2(.q(Q2), .d(xor2), .clk(clk), .en(enable), .clr(clr));
+
+xor xorGate1(xor1,Q2,xor2);
+
 dffe_ref D1(.q(Q1), .d(Q2), .clk(clk), .en(enable), .clr(clr));
 dffe_ref D0(.q(Q0), .d(Q1), .clk(clk), .en(enable), .clr(clr));
 
-xor xor0(zkPrior, u, Q2);
-xor xor1(zk, zkPrior, Q0);
+xor xorGate0(Zk,xor1,Q0);
+xor xorGate3(xor3,Q0,Q1);
+
+assign tIn = xor3;
+
+endmodule

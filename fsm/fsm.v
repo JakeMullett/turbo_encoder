d3e9c6ca7debe215f365bd383cae9d7b7abad3aa
Jacob Mullett <jtm51@duke.edu>
2019-03-05 15:10:19 -0500

    e

--- fsm/fsm.v
+++ fsm/fsm.v
@@ -2,49 +2,41 @@ module fsm(
 	data_valid, 
 	reset, 
 	clk,
-	length,
+	length_flag,
 	enable,
 	trellis_enable,
 	current_state,
 	clr);
 	
-	input data_valid, reset, clk;
-	input[8:0] length;
+	input data_valid, reset, clk, length_flag;
 	output reg enable, clr, trellis_enable;
 	
 	output reg[2:0] current_state;
-	reg[2:0] next_state;
-	reg[8:0] length_counter;
+	reg[13:0] length_counter;
 	
-	parameter INIT = 1;
 	parameter WAIT = 0;
 	parameter ENCODE = 3;
 	parameter TERMINATE = 2;
-	parameter BUFFER = 7;
+
+	wire[13:0] length;
+
+	assign length = length_flag ? 13'd6000 : 13'd1000;
 	
 	initial begin
 		length_counter <= 0;
 		current_state <= WAIT;
-		length_counter <= 0;
 		enable <= 0;
+		trellis_enable <= 0;
 	end
 	
 	always @(posedge clk) begin
 		if (reset) begin
 			length_counter <= 0;
 			current_state <= WAIT;
-			next_state <= WAIT;
-			length_counter <= 0;
 			enable <= 0;
+			trellis_enable <= 0;
 		end else begin
 			case (current_state)
-//				INIT: begin
-//						clr <= 1;
-//						enable <= 0;
-//						trellis_enable <= 0;
-//						next_state <= WAIT;
-//						current_state <= BUFFER;
-//						end
 				WAIT: begin
 						if (data_valid) begin
 							clr <= 0;
@@ -60,10 +52,14 @@ module fsm(
 							length_counter <= 0;
 							enable <= 0;
 							trellis_enable <= 1;
+							clr <= 1;
 							current_state <= TERMINATE;
 						end
 						end
 				TERMINATE: begin
+						if (length_counter == 1) begin
+							clr <= 0;
+						end
 						if (length_counter < 4) begin
 							length_counter <= length_counter + 1;
 						end else begin
@@ -71,18 +67,10 @@ module fsm(
 							enable <= 0;
 							trellis_enable <= 0;
 							current_state <= WAIT;
-							clr <= 1;
 						end
-						end
-				BUFFER: begin
-						current_state <= next_state;
 						end
 			endcase
 		end
 	end
-//	
-//	assign clr = current_state == WAIT;
-//	and enand(enable, current_state[0], current_state[1]);
-//	assign trellis_enable = current_state == TERMINATE;
 	
 endmodule
